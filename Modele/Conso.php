8acc67513ce0bde91f76f513a4fd01d84a1e6dfe
Kakayano <neo.herbigniaux@std.heh.be>
2024-12-18 15:53:32 +0100

    style:

--- Modele/Conso.php
+++ Modele/Conso.php
@@ -4,7 +4,7 @@ require_once "Modele/Modele.php";
 
 class Conso extends Modele {
     public function consoDernierJour(){
-        $sql = "SELECT  DATE_FORMAT(date_mesure, '%W''%d') AS date, valeur_litres FROM consommation_eau ORDER BY date_mesure";
+        $sql = "SELECT DATE_FORMAT(date_mesure, '%W %d/%m/%y') AS date, valeur_litres FROM consommation_eau ORDER BY date_mesure";
         $conso = $this->executerRequete($sql);
         return $conso;
     }
