2346b119322c3779da5ff60b4788a4e6bedb5626
Kakayano <neo.herbigniaux@std.heh.be>
2024-12-18 15:46:55 +0100

    feat: amélioration de consommation

--- Modele/Conso.php
+++ Modele/Conso.php
@@ -4,7 +4,7 @@ require_once "Modele/Modele.php";
 
 class Conso extends Modele {
     public function consoDernierJour(){
-        $sql = "SELECT SUM(valeur_litres) AS valeur FROM consommation_eau WHERE date_mesure >= '2024-06-15 12:00:00' AND date_mesure <= '2024-06-19 12:00:00'";
+        $sql = "SELECT  DATE_FORMAT(date_mesure, '%W''%d') AS date, valeur_litres FROM consommation_eau ORDER BY date_mesure";
         $conso = $this->executerRequete($sql);
         return $conso;
     }
--- Vue/vueConso.php
+++ Vue/vueConso.php
@@ -1,6 +1,30 @@
 <?php $this->titre = "Consommation Journalière"; ?>
-<?php
-    foreach($conso as $consos){
-        echo $consos['valeur'];
-    }
-?>
+<div id="graph">
+        <div class="graph">
+
+    </div>
+    <div class="date">
+    <?php
+        foreach($conso as $valeur){
+            $value = $valeur['valeur_litres'];
+            $date = $valeur['date'];
+            echo "<input type='hidden' value='$value'>";
+            echo "<p>$date</p>";
+        }
+    ?>   
+    </div>
+</div>
+
+<script>
+    const graph =document.querySelector('.graph');
+    const values = document.querySelectorAll('.date input');
+    console.log(values);
+
+    values.forEach(val => {
+        const box =document.createElement('div');
+        box.classList.add('percentage');
+        box.style.setProperty('--height', val.value / 10 + "px");
+        graph.appendChild(box);
+    });
+
+</script>
