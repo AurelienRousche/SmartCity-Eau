4c78e96bc91dafee5edf684fee74ce6f91735762
Kakayano <neo.herbigniaux@std.heh.be>
2024-12-19 08:36:39 +0100

    style: utilisation de js pour le style et le graphique

--- Modele/Conso.php
+++ Modele/Conso.php
@@ -4,8 +4,14 @@ require_once "Modele/Modele.php";
 
 class Conso extends Modele {
     public function consoDernierJour(){
-        $sql = "SELECT DATE_FORMAT(date_mesure, '%W %d/%m/%y') AS date, valeur_litres FROM consommation_eau ORDER BY date_mesure";
+        $sql = "SELECT DATE_FORMAT(date_mesure, '%W %d-%m-%y') AS date, valeur_litres FROM consommation_eau ORDER BY date_mesure";
         $conso = $this->executerRequete($sql);
         return $conso;
+    }
+
+    public function calcConso($startDate, $endDate){
+        $sql = "SELECT DATE_FORMAT(date_mesure, '%W %d/%m/%y') AS date, valeur_litres FROM consommation_eau WHERE date_mesure>=? AND date_mesure<=?";
+        $consos = $this->executerRequete($sql, array($startDate, $endDate));
+        return $consos;
     }
 }
--- Vue/vueConso.php
+++ Vue/vueConso.php
@@ -1,30 +1,74 @@
-<?php $this->titre = "Consommation Journalière"; ?>
-<div id="graph">
-        <div class="graph">
-
-    </div>
-    <div class="date">
+<?php $this->titre = "Consommation"; ?>
     <?php
         foreach($conso as $valeur){
             $value = $valeur['valeur_litres'];
             $date = $valeur['date'];
-            echo "<input type='hidden' value='$value'>";
-            echo "<p>$date</p>";
+            echo "<input name='valeur' type='hidden' value='$value'>";
+            echo "<input name='date' type='hidden' value='$date'>";
         }
-    ?>   
-    </div>
+    ?>
+<form action="index.php?action=changeconso" method="post">
+  <input type="date" id="start-date" name="start-date" min="<?=$firstDate?>" value="<?=isset($startDate)?$startDate:""?>">
+  <input type="date" id="end-date" name="end-date" value="<?=isset($endDate)?$endDate:""?>">
+  <input type="submit" name="showButton" value="show">
+</form>
+<div class="containerCanvas">
+ <canvas id="myChart"></canvas>
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-    const graph =document.querySelector('.graph');
-    const values = document.querySelectorAll('.date input');
-    console.log(values);
+    const values = document.querySelectorAll("input[name='valeur']");
+    const dates = document.querySelectorAll("input[name='date']");
+    let valuesList = [];
+    let datesList = [];
+    const colors = ['rgba(255, 99, 132, 0.7)',
+        'rgba(54, 162, 235, 0.7)',
+        'rgba(255, 206, 86, 0.7)',
+        'rgba(75, 192, 192, 0.7)',
+        'rgba(153, 102, 255, 0.7)',
+        'rgba(255, 159, 64, 0.7)'];
 
     values.forEach(val => {
-        const box =document.createElement('div');
-        box.classList.add('percentage');
-        box.style.setProperty('--height', val.value / 10 + "px");
-        graph.appendChild(box);
+        valuesList.push(val.value);
     });
 
+    dates.forEach(date => {
+        datesList.push(date.value);
+    })
+
+    const context = document.getElementById('myChart');
+
+  new Chart(context, {
+    type: 'bar',
+    data: {
+      labels:datesList,
+      datasets: [{
+        label: 'Consommation en m3',
+        data: valuesList,
+        backgroundColor: colors,
+        borderWidth: 1
+      }]
+    },
+    options: {
+    responsive: true,
+    maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  // changer automatiquement le min de end-date
+  const startDate =document.getElementById('start-date');
+  const endDate =document.getElementById('end-date');
+
+  startDate.addEventListener('change', function(){
+    endDate.min = this.value;
+
+    if (endDateInput.value < this.value) {
+      endDateInput.value = this.value;
+    }
+  });
 </script>
